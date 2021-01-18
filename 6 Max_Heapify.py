14d811a2944017e5e91d1761ad00f0e2fdde273e
mtdosterly <74225864+mtdosterly@users.noreply.github.com>
2021-01-18 12:38:23 -0600

    bring # of args in line with heapsort

--- 6 Max_Heapify.py
+++ 6 Max_Heapify.py
@@ -1,25 +1,25 @@
 def build_max_heap(array):
 
-    def max_heapify(array,index):
+    def max_heapify(array,index,size):
 
         left = 2*(index) + 1
         right = 2*(index) + 2
 
-        if left <= (len(array)-1) and array[left] > array[index]:
+        if left <= size and array[left] > array[index]:
             largest = left
         else:
             largest = index
 
-        if right <= (len(array)-1) and array[right] > array[largest]:
+        if right <= size and array[right] > array[largest]:
             largest = right
 
         if largest != index:
             array[index], array[largest] = array[largest], array[index]
-            max_heapify(array, largest)
+            max_heapify(array,largest,size)
 
     i = len(array)//2
 
     while i >= 0:
-        max_heapify(array, i)
+        max_heapify(array,i,len(array)-1)
         i -= 1
     return array
