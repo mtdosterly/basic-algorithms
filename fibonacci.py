f4fc59b795d1c77b5d53cf08f5120f65505f01f2
mtdosterly <74225864+mtdosterly@users.noreply.github.com>
2022-05-07 15:49:09 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,5 @@
 def fibnaive(n):
+    """worst way to do it"""
     if n < 0:
         return "Invalid input"
     elif n == 0:
@@ -6,9 +7,10 @@ def fibnaive(n):
     elif n == 1 or n == 2:
         return 1
     else:
-        return step(n-1) + step(n-2)
+        return fibnaive(n-1) + fibnaive(n-2)
 
 def fibdyn(n):
+    """better, but more time consuming to append to the list than just work within a list of predetermined size"""
     if n < 0:
         return "Invalid input"
     elif n <= 1:
@@ -18,3 +20,11 @@ def fibdyn(n):
         for num in range (2,n+1):
             fibs.append(fibs[num-2] + fibs[num-1])
         return fibs[n]
+
+def fib(len):
+    """optimal"""
+    lst = [0]*len
+    lst[1] = 1
+    for x in range(2,len):
+        lst[x] = lst[x-1] + lst[x-2]
+    return lst[len-1]
